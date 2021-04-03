ad628bd12de7ae85fd92671ba353126a7664ed4f
chaichai <chanchai9797@hotmail.com>
2021-04-03 22:31:18 +0700

    create

---
+++ src/components/Login.vue
@@ -0,0 +1,91 @@
+<template>
+<section class="section">
+
+  <div id="login" class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      LOGIN
+    </p>
+    
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <div class="container">
+  <div class="field ">
+  <p class="control has-icons-left has-icons-right">
+    <input class="input" type="email" placeholder="Email">
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+    <input class="input" type="password" placeholder="Password">
+    <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control">
+    <button class="button is-success">
+      Login
+    </button>
+  </p>
+</div>
+</div>
+    </div>
+  </div>
+</div>
+  
+</section>
+  
+<!-- <div class="container is-widescreen">
+  <div class="notification is-primary">
+    This container is <strong>fullwidth</strong> <em>until</em> the <code>$widescreen</code> breakpoint.
+  </div>
+</div> -->
+
+<!-- <section id="login">
+<form class="box">
+  <div class="field">
+    <label class="label">Email</label>
+    <div class="control">
+      <input class="input" type="email" placeholder="e.g. alex@example.com">
+    </div>
+  </div>
+
+  <div class="field">
+    <label class="label">Password</label>
+    <div class="control">
+      <input class="input" type="password" placeholder="********">
+    </div>
+  </div>
+
+  <button class="button is-primary">Sign in</button>
+</form>
+</section> -->
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#login {
+
+  list-style-type: none;
+  padding: 0;
+  display: inline-block;
+  margin: 0 100;
+  
+  text-align: left;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <nav
+      class="navbar is-link is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="http://eng.sut.ac.th/cpe/cpesut/webcpe/"
+          target="_blank"
+        >
+          <img src="@/assets/logo-cpe.png" alt="CPE logo" />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <!-- <a class="navbar-item">
+        Home
+      </a> -->
+
+          <router-link class="navbar-item" to="/"> Home </router-link>
+          <router-link class="navbar-item" to="/"> Advisor Profiles </router-link>
+          <router-link class="navbar-item" to="/"> Report an issue </router-link>
+        
+
+          <!-- <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"> More </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item"> About </a>
+              <a class="navbar-item"> Jobs </a>
+              <a class="navbar-item"> Contact </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item"> Report an issue </a>
+            </div>
+          </div> -->
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <router-link to="/login" class="button is-light"> Sign in </router-link>
+              <!-- <a class="button is-light">
+                <router-link to="/login"> Sign in </router-link>
+              </a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <hr />
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+</style>
