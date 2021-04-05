c08700917726c08c465d0c991aabf6d44303398f
cc.chanchai <chanchai9797@hotmail.com>
2021-04-05 18:53:14 +0700

    ui : sign up

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,69 +1,6 @@
 <template>
   <div class="columns">
     <div class="column"></div>
-    <!-- <section>
-      <div id="login" class="card">
-        <header class="card-header is-primary">
-          <p class="card-header-title">LOGIN</p>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            <div class="container">
-              <div class="field">
-                <p class="control has-icons-left has-icons-right">
-                  <input class="input" type="email" placeholder="Email" />
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                  </span>
-                  <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input class="input" type="password" :value="password" placeholder="Password" />
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                  </span>
-                </p>
-              </div>
-              <b-checkbox>Remember me</b-checkbox>
-              <div class="field">
-                <p class="control">
-                  <button class="button is-success">Login</button>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <br />
-
-    <section id="login">
-      <form class="box">
-        <div class="field">
-          <label class="label">Email</label>
-          <div class="control">
-            <input
-              class="input"
-              type="email"
-              placeholder="e.g. alex@example.com"
-            />
-          </div>
-        </div>
-
-        <div class="field">
-          <label class="label">Password</label>
-          <div class="control">
-            <input class="input" type="password" placeholder="********" />
-          </div>
-        </div>
-
-        <button class="button is-primary">Sign in</button>
-      </form>
-    </section> -->
     <div id="login" class="column is-one-quarter">
       <section class="section">
         <form action="">
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,77 +1,77 @@
 <template>
-  <section>
-        
-        <div id="form" class="columns">
-          <div class="column"></div>
-          <b-field position="is-centered">
-          <b-input placeholder="Search..." type="search" icon="magnify">
-          </b-input>
-          <p class="control">
-            <b-button label="Submit" type="is-info" />
-          </p>
-        </b-field>
-          <div class="column"></div>
-        </div>
-     
-        <div id="form" class="columns">
-          <div class="column"></div>
-          <div class="column is-one-quarter">
-            <b-field
-              label="Username"
-              :label-position="labelPosition"
-              type="is-success"
-              message="This username is available"
-            >
-              <b-input disabled value="johnsilver" maxlength="30"></b-input>
-            </b-field>
-          </div>
-          <div class="column"></div>
-        </div>
+  <div class="columns">
+    <div class="column"></div>
+    <div id="login" class="column is-two-fifths">
+      <section class="section">
+        <form action="">
+          <div class="panel is-primary" style="width: auto">
+            
+            <div class="panel-heading">
+              <p>Register</p>
+            </div>
+            <div id="form">
+              <div class="column">
+                <b-field position="is-centered">
+                  <b-input placeholder="Search Student ID" type="search" icon="magnify">
+                  </b-input>
+                  <p class="control">
+                    <b-button label="Submit" type="is-info" />
+                  </p>
+                </b-field>
+              </div>
+            </div>
 
-        <div id="form" class="columns">
-          <div class="column"></div>
-          <div class="column is-one-quarter">
-            <b-field
-              label="Password"
-              :label-position="labelPosition"
-              type="is-warning"
-            >
-              <b-input value="123" type="password" maxlength="30"></b-input>
-              <template #message>
-                <div>Password is too short</div>
-                <div>Password must have at least 8 characters</div>
-              </template>
-            </b-field>
-          </div>
-          <div class="column"></div>
-        </div>
+            <div id="form">
+              <div class="column">
+                <b-field
+                  label="Username"
+                  :label-position="labelPosition"
+                  type="is-success"
+                  message="This username is available"
+                >
+                  <b-input disabled value="johnsilver" maxlength="30"></b-input>
+                </b-field>
+              </div>
+            </div>
 
-        <div id="form" class="columns">
-          <div class="column"></div>
-          <div class="column is-one-quarter">
-            <b-field label="Name" :label-position="labelPosition">
-              <b-input value="Kevin Garvey"></b-input>
-            </b-field>
-          </div>
-          <div class="column"></div>
-        </div>
+            <div id="form">
+              <div class="column">
+                <b-field
+                  label="Password"
+                  :label-position="labelPosition"
+                  type="is-warning"
+                >
+                  <b-input value="123" type="password" maxlength="30"></b-input>
+                  <template #message>
+                    <div>Password is too short</div>
+                    <div>Password must have at least 8 characters</div>
+                  </template>
+                </b-field>
+              </div>
+            </div>
 
-        <div id="form" class="columns">
-          <div class="column"></div>
-          <div class="column is-one-quarter">
-            <b-field
-              label="Email"
-              :label-position="labelPosition"
-              type="is-danger"
-              message="This email is invalid"
-            >
-              <b-input type="email" value="john@" maxlength="30"> </b-input>
-            </b-field>
-          </div>
-          <div class="column"></div>
-        </div>
-    
-        <!-- <div id="form" class="columns">
+            <div id="form">
+              <div class="column">
+                <b-field label="Name" :label-position="labelPosition">
+                  <b-input value="Kevin Garvey"></b-input>
+                </b-field>
+              </div>
+            </div>
+
+            <div id="form">
+              <div class="column">
+                <b-field
+                  label="Email"
+                  :label-position="labelPosition"
+                  type="is-danger"
+                  message="This email is invalid"
+                >
+                  <b-input type="email" value="john@" maxlength="30"> </b-input>
+                </b-field>
+              </div>
+            </div>
+
+            <!-- <div id="form" class="columns">
           <div class="column"></div>
           <div class="column is-one-quarter">
             <b-field
@@ -111,8 +111,15 @@
           </div>
           <div class="column"></div>
         </div> -->
-        <b-button type="is-primary">Submit</b-button>
-  </section>
+            <div class="column">
+              <b-button type="is-primary">Submit</b-button>
+            </div>
+          </div>
+        </form>
+      </section>
+    </div>
+    <div class="column"></div>
+  </div>
 </template>
 
 <script>
